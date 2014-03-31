902302ceca64d81e955937a22de73c7da7b8e5c0
cskilbeck <github@charlie.skilbeck.com>
2014-03-31 22:44:45 +0100

    ..

--- TypeScriptTest/app.js
+++
@@ -1,7 +0,0 @@
-﻿window.onload = function () {
-    var el = document.getElementById('content');
-    var span = document.createElement('span');
-    el.appendChild(span);
-    span.innerText = Dictionary['pearl'];
-};
-//# sourceMappingURL=app.js.map

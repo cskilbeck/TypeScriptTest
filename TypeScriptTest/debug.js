77a8c460c14dbb5c60a1f549a976487cc3f79225
cskilbeck <github@charlie.skilbeck.com>
2014-04-01 18:42:26 +0100

    ..

---
+++ TypeScriptTest/debug.js
@@ -0,0 +1,25 @@
+﻿//////////////////////////////////////////////////////////////////////
+
+/*global document, Font, Fixedsys, console */
+
+//////////////////////////////////////////////////////////////////////
+
+var Debug = (function () {
+
+    var font = new Font(Fixedsys),
+
+        Debug = {
+
+            context: null,
+
+            text: function (x, y, str) {
+                font.drawText(this.context, x, y, str);
+            },
+
+            log: function (str) {
+                console.log(str);
+            }
+        };
+
+    return Debug;
+}());

f8de5066136c078a7ead074271c453cea101e4f5
cskilbeck <github@charlie.skilbeck.com>
2014-04-04 19:47:15 +0100

    ..

---
+++ TypeScriptTest/buttonlist.js
@@ -0,0 +1,22 @@
+﻿//////////////////////////////////////////////////////////////////////
+
+var ButtonList = (function () {
+    "use strict";
+
+    var ButtonList = function () {
+        SpriteList.call(this);
+        this.visible = true;
+    };
+
+    //////////////////////////////////////////////////////////////////////
+
+    return Util.extendClass(SpriteList, ButtonList, {
+
+        update: function () {
+            this.forEach(function (b) {
+                b.update();
+            });
+        }
+    });
+
+}());

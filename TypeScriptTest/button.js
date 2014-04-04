cb1a105542d5893bed1e0c909e56705663c54430
cskilbeck <github@charlie.skilbeck.com>
2014-04-04 16:45:08 +0100

    ..

---
+++ TypeScriptTest/button.js
@@ -0,0 +1,82 @@
+﻿//////////////////////////////////////////////////////////////////////
+
+var ButtonList = (function () {
+    "use strict";
+
+    var ButtonList = function () {
+        SpriteList.call(this);
+        this.visible = true;
+    };
+
+    ButtonList.prototype = Object.create(SpriteList.prototype);
+
+    ButtonList.prototype.update = function () {
+        this.forEach(function (b) {
+            b.update();
+        });
+    };
+
+    return ButtonList;
+
+}());
+
+//////////////////////////////////////////////////////////////////////
+
+var Button = (function () {
+    "use strict";
+
+    var idle = 0,
+        hover = 1,
+        pressed = 2;
+
+    function Button(img, x, y, clicked, context) {
+        Sprite.call(this, img);
+        this.x = x;
+        this.y = y;
+        this.enabled = true;
+        this.state = idle;
+        this.clicked = clicked;
+        this.context = context;
+    }
+
+    Button.prototype = Object.create(Sprite.prototype);
+
+    Button.prototype.update = function () {
+
+        this.transparency = this.enabled ? 255 : 128;
+
+        if (this.loaded() && this.visible && this.enabled) {
+            switch (this.state) {
+            case idle:
+                this.scaleX = 1;
+                this.scaleY = 1;
+                if (!Mouse.left.held && this.pick(Mouse.position)) {
+                    this.state = hover;
+                }
+                break;
+            case hover:
+                this.scaleX = 1.1;
+                this.scaleY = 1.1;
+                if (!this.pick(Mouse.position)) {
+                    this.state = idle;
+                } else if (Mouse.left.pressed) {
+                    this.state = pressed;
+                }
+                break;
+            case pressed:
+                this.scaleX = 1.25;
+                this.scaleY = 1.25;
+                if (!this.pick(Mouse.position)) {
+                    this.state = idle;
+                } else if (Mouse.left.released) {
+                    this.clicked.call(this.context);
+                    this.state = hover;
+                }
+                break;
+            }
+        }
+    };
+
+    return Button;
+
+}());
---
+++ TypeScriptTest/matrix.js
@@ -0,0 +1,72 @@
+﻿//////////////////////////////////////////////////////////////////////
+
+var Matrix = (function () {
+    "use strict";
+
+    //////////////////////////////////////////////////////////////////////
+
+    var Matrix = function () {
+        this.m = [1, 0, 0, 1, 0, 0];
+    };
+
+    //////////////////////////////////////////////////////////////////////
+
+    Matrix.prototype = {
+
+        //////////////////////////////////////////////////////////////////////
+
+        translate: function (x, y) {
+            this.m[4] += this.m[0] * x + this.m[2] * y;
+            this.m[5] += this.m[1] * x + this.m[3] * y;
+            return this;
+        },
+
+        //////////////////////////////////////////////////////////////////////
+
+        scale: function (x, y) {
+            this.m[0] *= x;
+            this.m[1] *= x;
+            this.m[2] *= y;
+            this.m[3] *= y;
+            return this;
+        },
+
+        //////////////////////////////////////////////////////////////////////
+
+        rotate: function (radians) {
+            var cos = Math.cos(radians),
+                sin = Math.sin(radians),
+                m11 = this.m[0] * cos + this.m[2] * sin,
+                m12 = this.m[1] * cos + this.m[3] * sin,
+                m21 = -this.m[0] * sin + this.m[2] * cos,
+                m22 = -this.m[1] * sin + this.m[3] * cos;
+            this.m[0] = m11;
+            this.m[1] = m12;
+            this.m[2] = m21;
+            this.m[3] = m22;
+            return this;
+        },
+
+        //////////////////////////////////////////////////////////////////////
+
+        apply: function (p) {
+            return {
+                x: p.x * this.m[0] + p.y * this.m[2] + this.m[4],
+                y: p.x * this.m[1] + p.y * this.m[3] + this.m[5]
+            };
+        },
+
+        //////////////////////////////////////////////////////////////////////
+
+        transformArray: function (p) {
+            var i;
+            for (i = 0; i < p.length; ++i) {
+                p[i] = this.apply(p[i]);
+
+            }
+        }
+    };
+
+    return Matrix;
+
+}());

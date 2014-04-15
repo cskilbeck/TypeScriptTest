99e3ef550c1924446637bb1c838ca451d620186c
cskilbeck <github@charlie.skilbeck.com>
2014-04-15 18:30:17 +0100

    ..

--- TypeScriptTest/debug.js
+++ TypeScriptTest/debug.js
@@ -18,8 +18,20 @@ chs.Debug = (function () {
             cursorY = 0;
         },
 
-        text: function (x, y, str) {
-            d.push(str, x, y);
+        text: function (x, y, things) {
+            var i,
+                s = "",
+                c = "",
+                a;
+            for (i = 2; i < arguments.length; ++i) {
+                a = arguments[i];
+                if (typeof a !== 'string') {
+                    a = a.toString();
+                }
+                s = s + c + a;
+                c = ",";
+            }
+            d.push(s, x, y);
         },
 
         print: function (str) {
@@ -28,6 +40,9 @@ chs.Debug = (function () {
         },
 
         draw: function () {
+            var x,
+                y,
+                str;
             context.setTransform(1, 0, 0, 1, 0, 0);
             while (d.length > 0) {
                 font.renderString(context, d.shift(), d.shift(), d.shift());

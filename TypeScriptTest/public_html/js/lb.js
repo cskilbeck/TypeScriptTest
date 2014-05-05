22e89578d07a22cc0d563a69af3ab1f2b8f1cf2c
cskilbeck <github@charlie.skilbeck.com>
2014-05-05 18:14:22 +0100

    ..

---
+++ TypeScriptTest/public_html/js/lb.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+
+    mtw.LeaderBoard = chs.Class({
+        inherit$: [chs.Drawable],
+
+        $: function (font) {
+            this.dimensions = { width: 200, height: chs.desktop.height };
+            this.topLabel = new chs.Label("NN of NN", font).setPosition(4, 1);
+            this.addChild(this.topLabel);
+
+            this.names = new chs.ClipRect(0, this.topLabel.height + 2, 170, chs.desktop.height - this.topLabel.height - 2);
+            this.addChild(this.names);
+
+            this.scores = new chs.ClipRect(172, this.names.)
+
+
+        },
+
+        update: function () {
+            // call webservice, get LB, update names and scores
+        }
+
+    });
+
+} ());

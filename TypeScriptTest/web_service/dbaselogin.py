1751f3365f3e82c435cbb6df64355dd1f1bc25f2
cskilbeck <github@charlie.skilbeck.com>
2014-05-05 18:57:01 +0100

    ..

---
+++ TypeScriptTest/web_service/dbaselogin.py
@@ -0,0 +1,11 @@
+def host():
+    return 'mtwdb.cnk16j9pzvyy.us-east-1.rds.amazonaws.com'
+
+def user():
+    return 'mtwuser'
+
+def password():
+    return 'mtwpassword'
+
+def db():
+    return 'mtwdb'

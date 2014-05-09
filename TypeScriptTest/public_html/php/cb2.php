674b9af28fdc9ab67f0e500e07fcab5919926d46
cskilbeck <github@charlie.skilbeck.com>
2014-05-09 07:25:33 +0100

    ..

---
+++ TypeScriptTest/public_html/php/cb2.php
@@ -0,0 +1,12 @@
+<?
+$oauth2_provider_id     = '2';
+$oauth2_client_id       = '224932627603132';
+$oauth2_secret          = 'e6f6e69b22e2c2b0b949a6f5611c3052';
+$oauth2_redirect        = 'http://make-the-words.com/oauth2callback';
+$oauth2_server_url      = 'https://www.facebook.com/dialog/oauth';
+$oauth2_scope           = '';
+$oauth2_response_type   = 'code';
+$oauth2_tokenurl        = 'https://graph.facebook.com/oauth/access_token';
+$oauth2_grant_type      = 'authorization_code';
+$oauth2_user_id_url     = 'https://apis.live.net/v5.0/me?access_token={ACCESS_TOKEN}';
+?>

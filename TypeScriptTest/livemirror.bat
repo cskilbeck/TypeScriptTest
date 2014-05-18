54fa4f829e6f3d1601677e754dd5ab6011d04c6c
cskilbeck <github@charlie.skilbeck.com>
2014-05-18 07:58:34 +0100

    ..

--- TypeScriptTest/livemirror.bat
+++ TypeScriptTest/livemirror.bat
@@ -7,9 +7,9 @@ WinSCP /command "open EC2" "synchronize remote ""%CURDIR%\web_service"" /usr/loc
 WinSCP /command "open EC2" "synchronize remote ""%CURDIR%\nodejs"" /home/ec2-user/nodejs" "exit"
 WinSCP /command "open EC2" "synchronize remote ""%CURDIR%\public_html\js"" /home/ec2-user/nodejs/js"  "exit"
 
-start /b WinSCP /command "open HostPapa" "keepuptodate ""%CURDIR%\public_html"" /home/maket847/public_html"
-start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\SQL"" /home/ec2-user/sql"
-start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\web_service"" /usr/local/www/wsgi-scripts"
-start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\nodejs"" /home/ec2-user/nodejs"
-start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\public_html\js"" /home/ec2-user/nodejs/js"
+rem start /b WinSCP /command "open HostPapa" "keepuptodate ""%CURDIR%\public_html"" /home/maket847/public_html"
+rem start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\SQL"" /home/ec2-user/sql"
+rem start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\web_service"" /usr/local/www/wsgi-scripts"
+rem start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\nodejs"" /home/ec2-user/nodejs"
+rem start /b WinSCP /command "open EC2" "keepuptodate ""%CURDIR%\public_html\js"" /home/ec2-user/nodejs/js"
 :End

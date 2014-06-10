772d756c77974648329a6416d2584e074bfb9cd7
cskilbeck <github@charlie.skilbeck.com>
2014-06-10 23:19:42 +0100

    ..

--- TypeScriptTest/public_html/js/tripple.js
+++ TypeScriptTest/public_html/js/tripple.js
@@ -15,25 +15,24 @@
             "rgb(255,0,0)",  // 2: red: deadly
             "rgb(0,0,255)"   // 3: blue: collectible    // (and 4: start position)
         ],
-        board,
+        board = [],
         // player
         score = 0,
         dead = false,
-        rotate,
+        rotate = false,
         angle = 0,
         rotateTime,
         currentRotation,
         targetRotation,
         standing = false,
-        space,
-        jump,
-        jumping,
+        space = false,
+        jump = false,
         lerpTime = 500,
         trans = [
-            { x: "x", y: "y", xm: 1, ym: 1},        // 0
-            { x: "y", y: "x", xm: 1, ym: -1},       // 90
-            { x: "x", y: "y", xm: -1, ym: -1},      // 180
-            { x: "y", y: "x", xm: -1, ym: 1},       // 270
+            { x: "xvel", y: "yvel", xm: 1, ym: 1},        // 0
+            { x: "yvel", y: "xvel", xm: 1, ym: -1},       // 90
+            { x: "xvel", y: "yvel", xm: -1, ym: -1},      // 180
+            { x: "yvel", y: "xvel", xm: -1, ym: 1}        // 270
         ];
 
     mtw.Player = chs.Class({inherit$: chs.Drawable,
@@ -42,14 +41,16 @@
             chs.Drawable.call(this);
             this.width = cell_width;
             this.height = cell_height;
-            this.setPosition(cell_width * 2 + 1, cell_height * 2);
+            this.setPosition(cell_width * 3, cell_height * 2);
             this.xvel = 0;
             this.yvel = 0;
         },
 
         onKeyDown: function(e) {
-            space = e.name === 'space';
-            jump = false;
+            if (standing && !rotate) {
+                space = e.name === 'space';
+                jump = false;
+            }
         },
 
         onKeyUp: function(e) {
@@ -59,7 +60,7 @@
             }
         },
 
-        check: function(x, y, contacts) {
+        check: function(x, y) {
             var p,
                 q,
                 u,
@@ -68,16 +69,15 @@
                 blocking = false;
             for (q = 0; q < 2; ++q) {
                 for (p = 0; p < 2; ++p) {
-                    u = ((x + p * this.width) / cell_width) >>> 0;
-                    v = ((y + q * this.height) / cell_height) >>> 0;
+                    u = ((x + p * (this.width - 1)) / cell_width) >>> 0;
+                    v = ((y + q * (this.height - 1)) / cell_height) >>> 0;
                     cell = board[u + v * board_width];
                     if (cell === 1) {
-                        blocking = true;
+                        return true;
                     }
-                    contacts.push({ x: u, y: v, cell: cell });
                 }
             }
-            return blocking;
+            return false;
         },
 
         move: function(deltaTime) {
@@ -86,76 +86,102 @@
                 u,
                 v,
                 cell,
+                tmp,
+                t,
                 mask = 0,
                 add = 1,
                 xvel,
                 yvel,
-                ho,
-                ve,
-                bo,
-                horiz = [],
-                vert = [],
-                both = [];
+                horiz,
+                vert,
+                both;
 
-            xvel = this.xvel;
-            yvel = this.yvel;
+            t = trans[angle];
 
-            ho = this.check(this.x + xvel, this.y, horiz);
-            ve = this.check(this.x, this.y + yvel, vert);
-            bo = this.check(this.x + xvel, this.y + yvel, both);
+            xvel = this[t.x] * t.xm;
+            yvel = this[t.y] * t.ym;
 
-            if (ho && !ve) {
+            if (xvel !== 0) {
+                horiz = this.check(this.x + xvel, this.y);
+            }
+            if (yvel !== 0) {
+                vert = this.check(this.x, this.y + yvel);
+            }
+            if (xvel !== 0 && yvel !== 0) {
+                both = this.check(this.x + xvel, this.y + yvel);
+            }
+
+            if (angle & 1) {
+                tmp = horiz;
+                horiz = vert;
+                vert = tmp;
+            }
+
+            if (horiz) {
                 xvel = 0;
                 yvel = 0;
                 currentRotation = -angle / 2 * Math.PI;
                 angle += 1;
                 targetRotation =  -angle / 2 * Math.PI;
+                angle &= 3;
+                space = false;
                 rotate = true;
-                rotateTime = chs.Timer.time + lerpTime;
-            } else if (ve && !ho) {
-                if (yvel > 0) {
-                    if (!standing) {
+                standing = true;
+                rotateTime = lerpTime;
+            } else {
+                if (vert) {
+                    if (yvel > 0) {
+                        if (!standing) {
+                            xvel = 0;
+                            console.log("Vertical!");
+                        }
+                        standing = true;
+                    }
+                    yvel = 0;
+                } else if (both) {
+                    if (yvel > 0) {
+                        standing = true;
                         xvel = 0;
                     }
-                    standing = true;
-                    jumping = false;
+                    yvel = 0;
                 }
-                yvel = 0;
-            } else if (ho && ve) {
-                xvel = 0;
-                yvel = 0;
-            } else if (bo) {
-                if (yvel > 0) {
-                    standing = true;
-                    jumping = false;
-                    xvel = 0;
-                }
-                yvel = 0;
             }
 
             this.x += xvel;
             this.y += yvel;
 
-            this.xvel = xvel;
-            this.yvel = yvel;
+            this[t.x] = xvel * t.xm;
+            this[t.y] = yvel * t.ym;
         },
 
         onUpdate: function(time, deltaTime) {
-            var dt = deltaTime;
-            if (space && standing === true && !rotate) {
-                this.xvel = 0.35;
-            } else if (jump && !jumping && !rotate) {
-                this.yvel = -0.85;
-                jump = false;
-                jumping = true;
-                standing = false;
-            }
+            var dt = deltaTime,
+                t = trans[angle];
+            chs.Debug.print("Standing: " + standing.toString());
+            chs.Debug.print("Rotate: " + rotate.toString());
+            chs.Debug.print("Angle: " + angle.toString());
+            chs.Debug.print("XVel: " + this.xvel.toString());
+            chs.Debug.print("YVel: " + this.yvel.toString());
+            chs.Debug.print("`XVel: " + this[t.x].toString());
+            chs.Debug.print("`YVel: " + this[t.y].toString());
             if (!rotate) {
-                while (dt > 0) {
-                    this.move(Math.min(1, dt));
-                    dt -= 1;
+                if (space && standing) {
+                    this.xvel = 0.35;
+                } else if (jump && standing) {
+                    this.yvel = -0.85;
+                    jump = false;
+                    standing = false;
                 }
-                this.yvel += deltaTime * gravity;
+                if (!rotate) {
+                    dt = Math.min(dt, 1000/20);
+                    while (dt > 0 && !rotate) {
+                        this.move(Math.min(1, dt));
+                        dt -= 1;
+                    }
+                    if (!rotate && !standing) {
+                        this.yvel += deltaTime * gravity;
+                    }
+                }
             }
         },
 
@@ -175,8 +201,6 @@
             this.setPivot(0.5, 0.5);
             this.setPosition(chs.desktop.width / 2, chs.desktop.height / 2);
 
-            board = [];
-
             // clear the board to white
             for (i = 0; i < board_size; ++i) {
                 board[i] = 0;
@@ -195,10 +219,10 @@
             }
 
             // now add the rest
-            board[2 + 5 * board_width] = 1;
-            board[2 + 6 * board_width] = 1;
             board[3 + 5 * board_width] = 1;
             board[3 + 6 * board_width] = 1;
+            board[4 + 5 * board_width] = 1;
+            board[4 + 6 * board_width] = 1;
 
             this.player = new mtw.Player();
             this.addChild(this.player);
@@ -209,13 +233,16 @@
             chs.Debug.print(Math.floor(this.rotation * 180 / Math.PI));
 
             if (rotate) {
-                dt = (rotateTime - time) / lerpTime;
-                if (dt <= 0) {
+                rotateTime -= deltaTime;
+                if (rotateTime <= 0) {
                     rotate = false;
-                    angle &= 3;
+                    this.xvel = 0;
+                    this.yvel = 0;
+                    standing = true;
+                    space = false;
                     this.rotation = -angle / 2 * Math.PI;
                 } else {
-                    dt = chs.Util.ease(chs.Util.ease(dt));
+                    dt = chs.Util.ease(chs.Util.ease(rotateTime / lerpTime));
                     this.rotation = targetRotation + (currentRotation - targetRotation) * dt;
                 }
             }

3f6f9fc01c63385f0193de02ecde22f7548ffa60
cskilbeck <github@charlie.skilbeck.com>
2014-06-13 10:05:02 +0100

    ..

--- TypeScriptTest/public_html/js/tripple.js
+++ TypeScriptTest/public_html/js/tripple.js
@@ -17,7 +17,7 @@
         ],
         board = [],
         // player
-        angle = 0,
+        player = null,
         startRotation,
         targetRotation,
         rotateStartTime,
@@ -26,6 +26,7 @@
         oldFlip = false,
         space = false,
         standing = false,
+        flying = false,
         jump = false;
 
     function fmod(x, y) {
@@ -52,8 +53,10 @@
         },
 
         onKeyDown: function(e) {
-            space = e.name === 'space';
-            jump = false;
+            if (!flip) {
+                space = e.name === 'space';
+                jump = false;
+            }
         },
 
         onKeyUp: function(e) {
@@ -90,7 +93,7 @@
                 ny = this.y + this.yvel * deltaTime,
                 ox,
                 oy,
-                landed = false,
+                wasFlying = flying,
                 mask = this.check(nx, ny);
             switch(mask) {
                 case 0:
@@ -112,6 +115,10 @@
                     if (ox < oy) {
                         nx = round_down(nx, cell_width);    // !!!
                         this.xvel = 0;
+                        this.yvel = 0;
+                        jump = false;
+                        space = false;
+                        flip = true;
                     } else {
                         ny = round_up(ny, cell_height);
                         this.yvel = 0;
@@ -130,7 +137,7 @@
                     } else {
                         ny = round_down(ny, cell_height);
                         this.yvel = 0;
-                        landed = true;
+                        flying = false;
                     }
                     break;
                 case 5:
@@ -138,6 +145,15 @@
                     this.xvel = 0;
                     break;
                 case 6:
+                    if (Math.atan2(this.xvel, this.yvel) > 0) {
+                        nx = round_up(nx, cell_width);
+                        ny = round_up(ny, cell_height);
+                    } else {
+                        nx = round_down(nx, cell_width);
+                        ny = round_down(ny, cell_height);
+                    }
+                    this.xvel = 0;
+                    this.yvel = 0;
                     break;
                 case 7:
                     nx = round_up(nx, cell_width);
@@ -151,11 +167,14 @@
                     if (oy > ox) {
                         nx = round_down(nx, cell_width);    // !!!
                         this.xvel = 0;
+                        this.yvel = 0;
+                        jump = false;
+                        space = false;
                         flip = true;
                     } else {
                         ny = round_down(ny, cell_height);
                         this.yvel = 0;
-                        landed = true;
+                        flying = false;
                     }
                     break;
                 case 9:
@@ -163,6 +182,9 @@
                 case 10:
                     nx = round_down(nx, cell_width);    // !!!
                     this.xvel = 0;
+                    this.yvel = 0;
+                    jump = false;
+                    space = false;
                     flip = true;
                     break;
                 case 11:
@@ -175,13 +197,14 @@
                 case 12:
                     ny = round_down(ny, cell_height);
                     this.yvel = 0;
-                    landed = true;
+                    flying = false;
                     break;
                 case 13:
                     nx = round_up(nx, cell_width);
                     ny = round_down(ny, cell_height);
                     this.xvel = 0;
                     this.yvel = 0;
+                    flying = false;
                     break;
                 case 14:
                     nx = round_down(nx, cell_width);    // !!!
@@ -189,38 +212,53 @@
                     this.xvel = 0;
                     this.yvel = 0;
                     flip = true;
+                    flying = false;
                     break;
                 case 15:
                     nx = this.x;
                     ny = this.y;
                     this.xvel = 0;
                     this.yvel = 0;
+                    flying = false;
                     break;
             }
             this.x = nx;
             this.y = ny;
+            if (flying) {
+                space = false;
+            } else if (wasFlying) {
+                this.xvel = 0;
+                jump = false;
+                space = false;
+            }
         },
 
         onUpdate: function(time, deltaTime) {
             var dt = deltaTime,
                 of = flip;
+            chs.Debug.print(Math.atan2(this.xvel, this.yvel));
             if (flip && !of) {
+                jump = false;
+                space = false;
+                flying = false;
                 this.xvel = 0;
                 this.yvel = 0;
             }
-            else if (!flip && this.yvel !== 0) {
+            else if (!flip) {
                 if (space) {
                     this.xvel = 0.5;
                 } else if (jump) {
-                    this.yvel = -0.5;
+                    this.yvel = -0.75;
+                    console.log("JUMP!!!!!!!!!!!");
                     jump = false;
+                    flying = true;
                 }
-            }
-            this.yvel += gravity * deltaTime;
-            chs.Debug.print(this.xvel.toFixed(2), this.yvel.toFixed(2));
-            while (dt > 0) {
-                this.move(Math.min(dt, 1));
-                dt -= 1;
+                this.yvel += gravity * deltaTime;
+                chs.Debug.print(this.xvel.toFixed(2), this.yvel.toFixed(2));
+                while (dt > 0) {
+                    this.move(Math.min(dt, 1));
+                    dt -= 1;
+                }
             }
         },
 
@@ -263,19 +301,20 @@
             board[4 + 5 * board_width] = 1;
             board[4 + 6 * board_width] = 1;
 
-            this.player = new mtw.Player();
-            this.addChild(this.player);
+            player = new mtw.Player();
+            this.addChild(player);
         },
 
         onUpdate: function(time, deltaTime) {
-            var dt;
+            var dt,
+                x,
+                y,
+                tmp;
             if (flip) {
                 if (!oldFlip) {
                     oldFlip = true;
-                    startRotation = angle * Math.PI * -0.5;
-                    angle += 1;
-                    targetRotation = angle * Math.PI * -0.5;
-                    angle &= 3;
+                    startRotation = 0;
+                    targetRotation = Math.PI * -0.5;
                     rotateStartTime = time;
                 }
                 dt = (time - rotateStartTime) / lerpTime;
@@ -284,10 +323,21 @@
                     this.rotation = startRotation + (targetRotation - startRotation) * dt;
                 } else {
                     // actually flip everything
-                    this.rotation = targetRotation;
+                    this.rotation = 0;
                     flip = false;
+                    tmp = [];
+                    for (y = 0; y < board_height; ++y) {
+                        for (x = 0; x < board_width; ++x) {
+                            tmp[(board_width - 1 - y) + x * board_width] = board[x + y * board_width];
+                        }
+                    }
+                    board = tmp;
+                    tmp = player.x;
+                    player.x = this.width - player.y - player.width;
+                    player.y = tmp;
                 }
             }
+            oldFlip = flip;
         },
 
         onDraw: function(context) {

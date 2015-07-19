d54fd000a86a2e75da869b35ddec0461ff73aac1
Charlie Skilbeck <github@charlie.skilbeck.com>
2015-07-19 23:04:24 +0100

    no message

--- TypeScriptTest/debug_sync.bat
+++ TypeScriptTest/debug_sync.bat
@@ -1,6 +1,6 @@
-@echo off
+::@echo off
 
 WinSCP /script=sync.scp /parameter // %CD% FatCow DigitalOceanDEBUG config_debug /skilbeck/mtw
 
-start /b WinSCP /command "open FatCow" "lcd %CD%" "option batch continue" "keepuptodate public_html /skilbeck/mtw" "exit"
-start /b WinSCP /command "open DigitalOceanDEBUG" "lcd %CD%" "option batch continue" "keepuptodate root /" "exit"
+start /b WinSCP /command "open FatCow" "lcd %CD%" "option batch continue" "keepuptodate public_html /skilbeck/mtw"
+start /b WinSCP /command "open DigitalOceanDEBUG" "lcd %CD%" "option batch continue" "keepuptodate root /"

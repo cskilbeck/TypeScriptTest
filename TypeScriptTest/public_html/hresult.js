8fb9503573f9a92329cc9c2ea9407cd94a568f7c
Charlie Skilbeck <github@charlie.skilbeck.com>
2015-08-05 11:37:46 +0100

    no message

--- TypeScriptTest/public_html/hresult.js
+++ TypeScriptTest/public_html/hresult.js
@@ -1,11 +1,7 @@
-function br(s) {
-    return s.replace(/\&\#10\;/g, "<br>");
-}
-
-//0x41010013
 angular.
     module("mainApp", ['ngSanitize']).
     controller('hresultController', function($scope, $timeout, $http, $sce) {
+        "option strict";
 
         var timer = null,
             webservice = "http://45.55.209.178/hresult?action=find&code=",
@@ -13,13 +9,13 @@ angular.
                 { bit: 31, name: "Severity", text:["SUCCESS","FAILURE"] },
                 { bit: 30, name: "Reserved", text: ["NORMAL", "SEVERE"] },
                 { bit: 29, name: "Owner", text: ["MICROSOFT", "CUSTOMER"] },
-                { bit: 28, name: "Is NT", text: ["NT", "NORMAL"] },
+                { bit: 28, name: "IsNT", text: ["NT", "NORMAL"] },
                 { bit: 27, name: "Type", text: ["STATUS CODE", "DISPLAY STRING"] }
             ];
 
         $scope.status = 'Ready';
         $scope.info = '';
-        $scope.query = '';
+        $scope.query = '401';
         $scope.result = [];
         $scope.tableVisible = $scope.result.length > 0;
         $scope.iconStyle = "glyphicon glyphicon-null";
@@ -33,6 +29,9 @@ angular.
                 return;
             }
             timer = $timeout(function() {
+                if(timer !== null) {
+                    $timeout.cancel(timer);
+                }
                 timer = null;
                 $scope.status = 'Searching...';
                 $scope.iconStyle = "glyphicon glyphicon-refresh gly-spin";
@@ -46,6 +45,7 @@ angular.
                             code,
                             more,
                             tip,
+                            e,
                             r = [];
                         $scope.status = data.status;
                         more = (data.numresults >= 10) ? " or more" : "";
@@ -53,22 +53,20 @@ angular.
                         for(i in data.errors) {
                             err = data.errors[i];
                             n = parseInt(err.number);
-                            tip = "<table class='table table-condensed table-bordered'>";
-                            for(j in msg) {
-                                m = msg[j];
-                                tip += "<tr><td><b>" + m.name + ":</b></td><td>" + m.text[(n >>> m.bit) & 1] + "</td></tr>";
-                            }
-                            code = n & 0xffff;
-                            tip += "<td><b>Facility:</b></td><td>" + err.facility + "</td></tr>";
-                            tip += "<td><b>Code:</b></td><td>0x" + code.toString(16) + "&nbsp;(" + code.toString() + ")</td></tr>";
-                            tip += "</table>";
-                            r.push({
+                            e = {
                                 error: err.error,
                                 name: err.name,
-                                description: $sce.trustAsHtml(br(err.description)),
-                                tooltip: "<b>" + err.error + "</b><br/>" + err.name,
-                                tooltipcontent: $sce.trustAsHtml(tip)
-                            });
+                                file: err.file,
+                                details: {}
+                            };
+                            for(j in msg) {
+                                m = msg[j];
+                                e.details[m.name] = m.text[(n >>> m.bit) & 1];
+                            }
+                            e.details.description = $sce.trustAsHtml(err.description.replace(/&#10;/g, "<br>"));
+                            e.details.facility = err.facility;
+                            e.details.code = err.error & 0xffff;
+                            r.push(e);
                         }
                         $scope.result = r;
                         $scope.tableVisible = $scope.result.length > 0;
@@ -83,6 +81,49 @@ angular.
                     });
             }, 500);
         });
+
+        $scope.rowExpanded = false;
+        $scope.rowExpandedCurr = "";
+        $scope.rowExpandedPrev = "";
+        $scope.rowExpandedID = "";
+
+        $scope.rowExpandFn = function() {
+            var i;
+            $scope.rowExpand = [];
+            for(i=0; i<$scope.result.length; ++i) {
+                $scope.rowExpand.push(false);
+            }
+        };
+
+        $scope.selectRow = function(index, code) {
+            if(typeof $scope.rowExpand === 'undefined') {
+                $scope.rowExpandFn();
+            }
+
+            if($scope.rowExpanded === false &&
+                    $scope.rowExpandedCurr === "" &&
+                    $scope.rowExpandedID === "") {
+                $scope.rowExpandedPrev = "";
+                $scope.rowExpanded = true;
+                $scope.rowExpandedCurr = index;
+                $scope.rowExpandedID = code.toString();
+                $scope.rowExpand[index] = true;
+            } else if($scope.rowExpanded === true) {
+                if($scope.rowExpandedCurr === index && $scope.rowExpandedID === code) {
+                    $scope.rowExpanded = false;
+                    $scope.rowExpandedCurr = "";
+                    $scope.rowExpandedID = "";
+                    $scope.rowExpand[index] = false;
+                } else {
+                    $scope.rowExpandedPrev = $scope.rowExpandedCurr;
+                    $scope.rowExpandedCurr = index;
+                    $scope.rowExpandedID = code.toString();
+                    $scope.rowExpand[$scope.rowExpandedPrev] = false;
+                    $scope.rowExpand[$scope.rowExpandedCurr] = true;
+                }
+            }
+
+        };
     }).directive('autoFocus', function($timeout) {
         return {
             restrict: 'AC',
@@ -108,3 +149,5 @@ angular.
             }
         };
     });
+
+//0x41010013
